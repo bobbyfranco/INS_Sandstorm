54e201308efae40c681cc421253ba87f9fdea112
bobbyfranco <wells.ej@yahoo.com>
2025-11-26 21:04:27 -0600

    Improved UI
    Just revised version number, since I didn't do it last update.

--- launcher.bat
+++ launcher.bat
@@ -3,7 +3,7 @@
 REM /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 :: Title: Insurgency Sandstorm Advanced Server Launcher
 :: Author: Bobby Franco
-:: Version: 2.0.46
+:: Version: 2.0.47
 :: Date: 11/24/2025
 :: Description: Setup and launch self-hosted dedicated server.
 REM /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -1379,3 +1379,4 @@ echo.
 echo Launching server...
 echo You may now close this window at anytime.
 InsurgencyServer.exe %launchCmd%
+
